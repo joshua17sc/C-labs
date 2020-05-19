c3350d76cbc0bd27e59a078687fdffe93bd789b2
Joshua17sc <Joshua17sc@gmail.com>
2020-05-19 00:08:45 -0400

    fixed strcopy and added strncopy

--- IC1labs/strcopy.c
+++ IC1labs/strcopy.c
@@ -1,42 +1,60 @@
 #include <stdio.h>
 
-char *stringcopy(char *destination, char *source);
+char *stringcopy(char *dest, char *src);
 
-int main(){
+char *stringncopy(char *dest, const char *src, size_t n);
 
-char oldstring[] = "Hello Icy One!";
-printf("%s\n",oldstring);
-stringcopy(oldstring, "Replacement!");
-printf("%s\n",oldstring);
-char newstring[] = "Still works.";
-stringcopy(oldstring, newstring);
-printf("%s\n",oldstring);
-return 0;
+int main()
+{
+
+	char oldstring[] = "Hello Icy One!";
+	printf("%s\n",oldstring);
+
+	stringcopy(oldstring, "Replacement!");
+	printf("%s\n",oldstring);
+
+	char newstring[] = "Still works.";
+	stringcopy(oldstring, newstring);
+	printf("%s\n",oldstring);
+
+	char nstring[12];
+	stringncopy(nstring, oldstring, 8);
+	printf("%s\n",nstring);
+	return 0;
 }
 
-char *stringcopy(char *destination, char *source){
-    /*  Description: I copy a string from one location to another.
-        destination - char * destination of copied string
-        source  - char * source of copied string
-        returns - char * 
-    */
+char *stringcopy(char *dest, char *src)
+{
+    //Copies a string from one location to another in memory.
+    size_t i = 0;    
+	char *start = &dest[0];
+	//our return value is the address of the beginning of our new string
+	while(*src) *dest++=*src++;
+	//copy each character over while there is a character left in the source
+		
+	*dest++ = 0;
+	//End the new string with a null byte.
+	return start;
+	//returns the address of the beginning of the new string.   
+}
+char *stringncopy(char *dest, const char *src, size_t n)
+{
+    //Copies a string from one location to another in memory, or up to n number of characters from source.
     
-    char *start=destination; 
-    // Creates a temporary variable as starting point for the new array/string. Needed for return.
-    
-    while(*source != '\0') 
-    // This will make sure we get all of the characters in *source.
-    {
-        
-        *destination = *source; 
-        // Copy the character from * source to * destination.
-        destination++; 
-        // Iterate the address for *destination.
-        source++; 
-        // Iterate the address for *source.
-    }
-    *destination = '\0'; 
-    // Finish the array/string with null character since we did not copy it over and our source and destination may not be the same length.
-    return start; 
-    // Returns the address to start the copied string.
+    size_t i = 0;    
+	char *start = &dest[0];
+	//our return value is the address of the beginning of our new string
+	while(*src && i < n) 
+	{
+		*dest++=*src++;
+		//copy each character over while there is a character left in the source
+		i++;
+		//Iterate i for loop n scenario.
+	}
+	*dest++ = 0;
+	//End the new string with a null byte.
+	for(; i < n; i++) *dest++=0;
+	//Fills remaining n space with 0's.
+	return start;
+	//returns the address of the beginning of the new string.   
 }
